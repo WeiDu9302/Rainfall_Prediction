d3fc7c87fe985f45b7f1ac09275d5acbfc61fbec
Wei Du <weidu9302@gmail.com>
2025-03-28 04:08:26 +0000

    Update server_rain.py

--- server-client/server_rain.py
+++ server-client/server_rain.py
@@ -111,4 +111,4 @@ def predict_rainfall():
 
 # ========== Run the Server ==========
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
